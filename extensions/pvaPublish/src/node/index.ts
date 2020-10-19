8ac7e429f6eb8c61303b6d9e7ba15258ff3ccfe4
Tony Anziano <tonyanziano5@gmail.com>
2020-10-19 15:41:53 -0700

    Changed pva publish registration to new API

--- extensions/pvaPublish/src/node/index.ts
+++ extensions/pvaPublish/src/node/index.ts
@@ -2,7 +2,8 @@ import { getStatus, history, publish, pull } from './publish';
 
 function initialize(registration) {
   const plugin = {
-    customDescription: 'Publish bot to Power Virtual Agents (Preview)',
+    name: 'pvaPublish',
+    description: 'Publish bot to Power Virtual Agents (Preview)',
     bundleId: 'publish',
     history,
     getStatus,
